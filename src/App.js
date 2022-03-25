eed48ab6211d1514c747ef6f600be329d88bdc23
Landon Phillips <landonphillips@Landons-MBP.attlocal.net>
2022-03-25 12:51:33 -0500

    finished sprint mvp, style included

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([{
+    id: 1
+  }])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -20,15 +23,12 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">REACT WARS</h1>
+      <h1 className="Header">REACT WARS  </h1>
       <section>
       {
         characters.map(characterObj => {
           return (
-            <article>
-            <h2>{characterObj.name}</h2>
-            <p>{characterObj.birth_year}</p>
-          </article>
+           <Character  key={characterObj.id} characterObj={characterObj} />
           )
         })
       }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,51 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledCharacter = styled.article`
+
+display: flex;
+justify-content: space-between;
+align-items: center;
+align-conent: center;
+border: 2px  solid #a4c090;
+margin-top: 3%;
+margin-right: 23%;
+margin-left: 23%;
+color: white;
+-webkit-text-stroke: 1px black;
+background-color: rgb(0,0,0, .3);
+
+h2{
+    text-shadow: 2px 2px black;
+}
+
+
+p {
+    justify-content: center;
+    align-items: center;
+    border: 1px solid white;
+    font-size: 1.5rem;
+    border-radius: 10px;
+    background-color: #808080;
+    
+}
+
+
+`
+
+const Character = (props) => {
+    const {characterObj} = props
+    
+    return(
+        
+         <StyledCharacter>
+            <h2>{characterObj.name}</h2>
+            <p>{characterObj.birth_year}</p>
+          </StyledCharacter>
+        
+
+    )
+}
+
+export default Character
